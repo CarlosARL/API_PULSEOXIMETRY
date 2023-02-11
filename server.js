c8e632a567145242be70fa0571a072fd69a9d632
CarlosARL <carlisdjandres6@gmail.com>
2023-02-10 23:14:38 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("message", (data) => {
+    console.log("Received message:", data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
