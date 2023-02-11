a4060cb3e6815a009f0b0c3189fbe4d0cf6bb29e
Carlos Ramos <55562334+CarlosARL@users.noreply.github.com>
2023-02-11 01:29:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    if (req.method === 'GET' && req.url === '/data') {
-        console.log('Received data:');
-        req.on('data', function(chunk) {
-            console.log(chunk.toString());
-        });
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Data received.');
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not found.');
-    }
+const http = require('http');
+const url = require('url');
+const server = http.createServer(function (req, res) {
+  const query = url.parse(req.url, true).query;
+  const valor = query.valor;
+  console.log("Received value: " + valor);
 });
-var port = 5555;
-server.listen(port, function() {
-    console.log('Listening on port ' + port);
-});
+
+const port = 5555;
+server.listen(port);
+console.log("Server listening on port " + port);
